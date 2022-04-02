792e0c576354be4c02068ab62080783cba3ef4fb
Michał <KrzyzowskiM1998@gmail.com>
2022-04-02 11:02:02 +0200

    passing values and references to function

--- script.js
+++ script.js
@@ -26,3 +26,29 @@ createBooking('LH123', 2, 800);
 createBooking('LH123', 2);
 createBooking('LH123', 5);
 createBooking('LH123', undefined, 1000);
+
+const flight = 'LH1234';
+const jonas = {
+  name: 'Jonas Schmiddtmean',
+  passport: 34567834342,
+};
+
+const checkIn = function (flightNum, passanger) {
+  flightNum = 'LH999';
+  passanger.name = `Mr. ${passanger.name}`;
+
+  if (passanger.passport === 34567834342) {
+    alert('Checked in');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+// checkIn(flight, jonas); //flight primitve - copy, jonas ref object - ref to object on heap
+console.log(flight);
+console.log(jonas);
+
+const newPassanger = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000000000);
+};
+newPassanger(jonas);
+checkIn(flight, jonas);
