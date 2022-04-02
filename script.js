50909f4862fd1b9eb6103d01278ebdb5c8237ce2
Michał <KrzyzowskiM1998@gmail.com>
2022-04-02 10:40:49 +0200

    Function - defualt parameters

---
+++ script.js
@@ -0,0 +1,28 @@
+'use strict';
+const bookings = [];
+
+const createBooking = function (
+  flightNum,
+  numPassangers = 1,
+  price = 199 * numPassangers
+) {
+  //es5 doing defualt value
+  //   numPassangers = numPassangers || 1;
+  //   price = price || 199;
+
+  const booking = {
+    flightNum,
+    numPassangers,
+    price,
+  };
+
+  console.log(booking);
+
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH123', 2, 800);
+createBooking('LH123', 2);
+createBooking('LH123', 5);
+createBooking('LH123', undefined, 1000);
